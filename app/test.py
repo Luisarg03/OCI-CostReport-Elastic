8fc5403002fb0650ad20ba106263644c2bdca3de
luisarg03 <luis.m.paz.03@gmail.com>
2021-10-01 13:22:58 -0300

    add fields oci

--- app/test.py
+++ app/test.py
@@ -8,12 +8,14 @@ file_location = '../OciConfig/config.prod'
 config = oci.config.from_file(file_location=file_location, profile_name='NC')
 object_storage_client = oci.object_storage.ObjectStorageClient(config)
 reporting_bucket = config['tenancy']
+fields = "name, size, timeCreated"
 report_bucket_objects = object_storage_client.list_objects(
                                                         reporting_namespace,
                                                         reporting_bucket,
                                                         prefix=prefix_file,
                                                         start_after=None,
-                                                        end=None)
+                                                        end=None,
+                                                        fields=fields)
 list_init = []
 for o in report_bucket_objects.data.objects:
     list_init.append({'name': o.name, 'size': o.size, 'time_created': o.time_created})
