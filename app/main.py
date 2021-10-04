ffccd47a3b95b83f26903190e59d4d2c5f84801d
luisarg03 <luis.m.paz.03@gmail.com>
2021-10-04 12:27:09 -0300

    fix

--- app/main.py
+++ app/main.py
@@ -43,18 +43,14 @@ if __name__ == '__main__':
     '''
     prefix = "reports/cost-csv"
     start_after = lastupdate(esclient, index)
-    print('\n Ultimo file en Elastic: ', index, start_after)
-
-    ### Obtengo el nombre completo del file desde donde iniciar la descarga...
     start_after = getfiles(ociclient['client'], ociclient['bucket'], start_after)
-    print('\n Nombre del files en bucket:', 'start ->',start_after)
+    print('\n Ultimo file en Elastic: ', index, start_after)
+    
 
     ##############################
     ### Variables de reproceso ###
-    # filename = '1000000692115.csv.gz'
-    # prefix = getfiles(ociclient['client'], ociclient['bucket'], filename)
-    # start_after = None
-    # print('\n Reprocesado de files en bucket:', 'file ->',prefix)
+    prefix = 'reports/cost-csv/0001000000016203.csv.gz'
+    start_after = None
     ##############################
 
 
@@ -72,4 +68,4 @@ if __name__ == '__main__':
     PreProcess(path, list_files)
 
     ### Desa funcion
-    insert(path, esclient, index)
+    # insert(path, esclient, index)
